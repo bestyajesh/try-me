a48e2ad306a9d22144e77521c4c48bba86000771
bestyajesh <bestyajesh@gmail.com>
2020-08-18 19:33:40 +0530

    changed the spaces

--- sketch.js
+++ sketch.js
@@ -15,8 +15,6 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
-    
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -62,6 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
-
     platform.display();
 }
